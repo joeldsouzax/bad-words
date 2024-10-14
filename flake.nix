2224d0b14a47ab519543ac0b0679f6be2ccabe78
Joel DSouza <joeldsouzax@gmail.com>
2024-10-14 13:35:05 +0200

    added compose and rust build deps

--- flake.nix
+++ flake.nix
@@ -1,14 +1,23 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
-  };
 
+  description = "mono repo with rust backend with cargo workspaces";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpks-unstable";
+    utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-annalyzer-src.follows = "";
+      };
+    };
+  };
   outputs = { nixpkgs, utils, ... }:
     utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in with pkgs; {
         devShell.default =
-          mkShell { buildInputs = [ podman podman-compose dive jq ]; };
+          mkShell { buildInputs = [ podman podman-compose dive ]; };
       });
 }
