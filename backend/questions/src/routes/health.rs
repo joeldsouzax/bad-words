14609c3928ad298a8ff2e2ab8b6ac464bb749029
Joel D'Souza <joeldsouzax@gmail.com>
2025-06-05 23:23:56 +0200

    Update health.rs

--- backend/questions/src/routes/health.rs
+++ backend/questions/src/routes/health.rs
@@ -3,6 +3,6 @@ use warp::{http::StatusCode, Rejection, Reply};
 
 #[instrument]
 pub async fn health_check() -> Result<impl Reply, Rejection> {
-    event!(name: "health check", Level::INFO, "health check route handler");
+    
     Ok(warp::reply::with_status("healthy", StatusCode::OK))
 }
